498a1e9d3d1dfd072244ce216d8c7fe49d1ba376
D.J. Figueroa <djfigs10@gmail.com>
2023-01-12 19:23:32 -0500

    Version 0.1.7

---
+++ Runtime/Orientation.cs
@@ -0,0 +1,150 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Tactile
+{
+    [Serializable]
+    public struct Orientation
+    {
+        public Vector3 position;
+        public Quaternion rotation;
+        public Vector3 scale;
+
+        public static readonly Orientation Origin = new Orientation(Vector3.zero, Quaternion.identity, Vector3.one);
+        
+        public Orientation(Vector3 position, Quaternion rotation, Vector3 scale)
+        {
+            this.position = position;
+            this.rotation = rotation;
+            this.scale = scale;
+        }
+
+        public static Orientation FromTransform(Transform t, bool useLocalOrientation)
+        {
+            return useLocalOrientation ? LocalFromTransform(t) : WorldFromTransform(t);
+        }
+
+        public static Orientation LocalFromTransform(Transform t)
+        {
+            return new Orientation(t.localPosition, t.localRotation, t.localScale);
+        }
+        
+        public static Orientation WorldFromTransform(Transform t)
+        {
+            return new Orientation(t.position, t.rotation, t.localScale);
+        }
+
+        public void ApplyToTransformLocally(Transform t)
+        {
+            t.localPosition = position;
+            t.localRotation = rotation;
+            t.localScale = scale;
+        }
+
+        public void ApplyToTransformInWorldSpace(Transform t)
+        {
+            t.position = position;
+            t.rotation = rotation;
+            t.localScale = scale;
+        }
+
+        public void ApplyToTransform(Transform t, bool useLocalSpace)
+        {
+            if (useLocalSpace)
+            {
+                ApplyToTransformLocally(t);
+            }
+            else
+            {
+                ApplyToTransformInWorldSpace(t);
+            }
+        }
+
+        public Orientation SetPosition(Func<Vector3, Vector3> changePositionFunc)
+        {
+            position = changePositionFunc(position);
+            return this;
+        }
+
+        public Orientation SetPosition(Vector3 pos)
+        {
+            position = pos;
+            return this;
+        }
+        
+        public Orientation SetScale(Func<Vector3, Vector3> changeScaleFunc)
+        {
+            scale = changeScaleFunc(scale);
+            return this;
+        }
+
+        public Orientation SetScale(Vector3 scale)
+        {
+            this.scale = scale;
+            return this;
+        }
+        
+        public Orientation SetRotation(Func<Quaternion, Quaternion> changeRotationFunc)
+        {
+            rotation = changeRotationFunc(rotation);
+            return this;
+        }
+
+        public Orientation SetRotation(Quaternion rot)
+        {
+            rotation = rot;
+            return this;
+        }
+        
+        public static IEnumerator LerpOrientationCoroutineWithArc(Orientation from, Orientation to, Vector3 arcAxis, float arcHeight, IEnumerator<float> lerpEnumerator, Action<Orientation> orient)
+        {
+            yield return LerpOrientationCoroutine(from, to, lerpEnumerator, (orientation, t) =>
+            {
+                orientation.position += arcAxis * (Mathf.Sin(Mathf.PI * t) * arcHeight);
+                orient(orientation);
+            });
+        }
+        
+        public static IEnumerator LerpOrientationCoroutine(Orientation from, Orientation to, IEnumerator<float> lerpEnumerator, Action<Orientation> orient)
+        {
+            yield return LerpOrientationCoroutine(from, to, lerpEnumerator, (orientation, _) => orient(orientation));
+        }
+
+        public static IEnumerator LerpOrientationCoroutine(Orientation from, Orientation to, IEnumerator<float> lerpEnumerator, Action<Orientation, float> orient)
+        {
+            yield return TactileUtility.UseLerpEnumeratorCoroutine(lerpEnumerator, t =>
+            {
+                Orientation lerp = new Orientation()
+                {
+                    position = Vector3.Lerp(from.position, to.position, t),
+                    rotation = Quaternion.Lerp(from.rotation, to.rotation, t),
+                    scale = Vector3.Lerp(from.scale, to.scale, t)
+                };
+
+                orient(lerp, t);
+            });
+        }
+        
+        public static IEnumerator SlerpOrientationCoroutine(Orientation from, Orientation to, IEnumerator<float> lerpEnumerator, Action<Orientation> orient)
+        {
+            yield return TactileUtility.UseLerpEnumeratorCoroutine(lerpEnumerator, t =>
+            {
+                Orientation lerp = new Orientation()
+                {
+                    position = Vector3.Slerp(from.position, to.position, t),
+                    rotation = Quaternion.Slerp(from.rotation, to.rotation, t),
+                    scale = Vector3.Slerp(from.scale, to.scale, t)
+                };
+
+                orient(lerp);
+            });
+        }
+
+        public static implicit operator Orientation((Vector3, Quaternion, Vector3) tuple)
+        {
+            return new Orientation(tuple.Item1, tuple.Item2, tuple.Item3);
+        }
+    }
+}
--- Runtime/TactileUtility.cs
+++ Runtime/TactileUtility.cs
@@ -21,28 +21,79 @@ namespace Tactile
             Quaternion targetLocalRotation, Vector3 targetLocalScale, float time, bool slerp = false,
             CancelToken token = null)
         {
+            yield return LocallyLerpToCoroutine(transform,
+                new Orientation(targetLocalPosition, targetLocalRotation, targetLocalScale), time, slerp, token);
+        }
+
+        /// <summary>
+        /// Linearly lerps a transform to a specified position, orientation, and scale over a given time frame.
+        /// </summary>
+        /// <param name="transform">The transform to manipulate</param>
+        /// <param name="targetOrientation">The target orientation</param>
+        /// <param name="time">The time to translate</param>
+        /// <param name="slerp">Whether to slerp or not</param>
+        /// <param name="token">The cancel token to use</param>
+        public static IEnumerator LocallyLerpToCoroutine(this Transform transform, Orientation targetOrientation,
+            float time, bool slerp = false,
+            CancelToken token = null)
+        {
+            yield return LerpToCoroutine(transform, targetOrientation, time, true, slerp, token);
+        }
+
+        public static IEnumerator LocallyLerpToWithArcCoroutine(this Transform transform, Orientation targetOrientation,
+            Vector3 arcAxis, float arcHeight, float time, CancelToken token = null)
+        {
+            yield return LerpToWithArcCoroutine(transform, targetOrientation, arcAxis, arcHeight, time, true, token);
+        }
+
+        public static IEnumerator LerpToWithArcCoroutine(this Transform transform, Orientation targetOrientation,
+            Vector3 arcAxis, float arcHeight, float time, bool local = false, CancelToken token = null)
+        {
+            Orientation from = Orientation.FromTransform(transform, local);
+            var linerLerpEnumerator = CreateLinearLerpEnumerator(time, token);
+
+            void Orient(Orientation orientation)
+            {
+                orientation.ApplyToTransform(transform, local);
+            }
+
+            yield return Orientation.LerpOrientationCoroutineWithArc(from, targetOrientation, arcAxis, arcHeight, linerLerpEnumerator, Orient);
+        }
+
+        /// <summary>
+        /// Linearly lerps a transform to a specified position, orientation, and scale over a given time frame.
+        /// </summary>
+        /// <param name="transform">The transform to manipulate</param>
+        /// <param name="targetOrientation">The target orientation</param>
+        /// <param name="time">The time to translate</param>
+        /// <param name="local">Whether to lerp in the local transform space</param>
+        /// <param name="slerp">Whether to slerp or not</param>
+        /// <param name="token">The cancel token to use</param>
+        public static IEnumerator LerpToCoroutine(this Transform transform, Orientation targetOrientation,
+            float time, bool local = false, bool slerp = false,
+            CancelToken token = null)
+        {
             if (transform != null && transform)
             {
-                // Store starting position and orientation.
-                Vector3 startPos = transform.localPosition;
-                Quaternion startRot = transform.localRotation;
-                Vector3 startScale = transform.localScale;
-
-                yield return LinearLerpOverTimeCoroutine(time, t =>
+                void Orient(Orientation orientation)
                 {
-                    transform.localPosition =
-                        slerp
-                            ? Vector3.Slerp(startPos, targetLocalPosition, t)
-                            : Vector3.Lerp(startPos, targetLocalPosition, t);
-                    transform.localRotation =
-                        slerp
-                            ? Quaternion.Slerp(startRot, targetLocalRotation, t)
-                            : Quaternion.Lerp(startRot, targetLocalRotation, t);
-                    transform.localScale =
-                        slerp
-                            ? Vector3.Slerp(startScale, targetLocalScale, t)
-                            : Vector3.Lerp(startScale, targetLocalScale, t);
-                }, token);
+                    orientation.ApplyToTransform(transform, local);
+                }
+
+                Orientation from = Orientation.FromTransform(transform, local);
+                
+                var linearLerpEnumerator = CreateLinearLerpEnumerator(time, token);
+
+                if (slerp)
+                {
+                    yield return Orientation.SlerpOrientationCoroutine(from, targetOrientation, linearLerpEnumerator,
+                        Orient);
+                }
+                else
+                {
+                    yield return Orientation.LerpOrientationCoroutine(from, targetOrientation, linearLerpEnumerator,
+                        Orient);
+                }
             }
             else
             {
@@ -50,47 +101,6 @@ namespace Tactile
             }
         }
         
-        /// <summary>
-        /// Linearly lerps a transform to a specified position, orientation, and scale over a given time frame.
-        /// </summary>
-        /// <param name="transform">The transform to manipulate</param>
-        /// <param name="targetPosition">The target location position</param>
-        /// <param name="targetRotation">The target location rotation</param>
-        /// <param name="targetLocalScale">The target scale</param>
-        /// <param name="time">The time to translate</param>
-        public static IEnumerator LerpToCoroutine(this Transform transform, Vector3 targetPosition,
-            Quaternion targetRotation, Vector3 targetLocalScale, float time, bool slerp = false,
-            CancelToken token = null)
-        {
-            if (transform != null && transform)
-            {
-                // Store starting position and orientation.
-                Vector3 startPos = transform.position;
-                Quaternion startRot = transform.rotation;
-                Vector3 startScale = transform.localScale;
-
-                yield return LinearLerpOverTimeCoroutine(time, t =>
-                {
-                    transform.position =
-                        slerp
-                            ? Vector3.Slerp(startPos, targetPosition, t)
-                            : Vector3.Lerp(startPos, targetPosition, t);
-                    transform.rotation =
-                        slerp
-                            ? Quaternion.Slerp(startRot, targetRotation, t)
-                            : Quaternion.Lerp(startRot, targetRotation, t);
-                    transform.localScale =
-                        slerp
-                            ? Vector3.Slerp(startScale, targetLocalScale, t)
-                            : Vector3.Lerp(startScale, targetLocalScale, t);
-                }, token);
-            }
-            else
-            {
-                Debug.LogError("Tried to lerp a non-existent Transform!");
-            }
-        }
-
         public static IEnumerator LinearScaleCoroutine(this Transform transform, Vector3 targetLocalScale, float time,
             CancelToken token = null)
         {
@@ -213,6 +223,37 @@ namespace Tactile
             yield return LerpFuncOverTimeCoroutine(time, elapsed => elapsed / time, action, token);
         }
 
+        public static IEnumerator LerpFunctionCoroutine(float time, Func<float, float> lerpFunction,
+            Action<float> action, CancelToken token = null)
+        {
+            float elapsed = 0f;
+            while (token && elapsed <= time)
+            {
+                // Calculate lerp from lerp function.
+                float t = lerpFunction(elapsed);
+
+                // Perform action using lerp.
+                action(t);
+
+                // Wait extra frame and elapse time.
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            // Give a lerp value of 1 so that it always finishes in a completed state.
+            action(1f);
+        }
+
+        /// <summary>
+        /// Creates a linear lerp enumerator.
+        /// </summary>
+        /// <param name="time">The time over which to perform the lerp</param>
+        /// <param name="token">A cancel token to use for the enumerator</param>
+        public static IEnumerator<float> CreateLinearLerpEnumerator(float time, CancelToken token = null)
+        {
+            return CreateTimeBasedLerpEnumerator(time, elapsed => elapsed / time, token);
+        }
+
         /// <summary>
         /// Performs an action over time using a specified lerp function.
         /// </summary>
@@ -238,6 +279,46 @@ namespace Tactile
 
             // Give a lerp value of 1 so that it always finishes in a completed state.
             action(1f);
+        }
+
+        /// <summary>
+        /// Uses a lerp enumerator and performs a specified action with the lerp value until the lerp is complete.
+        /// </summary>
+        /// <param name="lerpEnumerator">The enumerator to generate lerp values.</param>
+        /// <param name="action">The action to perform with the lerp value</param>
+        public static IEnumerator UseLerpEnumeratorCoroutine(IEnumerator<float> lerpEnumerator, Action<float> action)
+        {
+            while (lerpEnumerator.MoveNext())
+            {
+                // Get the t value from the lerp function.
+                float t = lerpEnumerator.Current;
+
+                // Perform the action.
+                action(t);
+
+                // Wait for next frame.
+                yield return null;
+            }
+        }
+
+        public static IEnumerator<float> CreateTimeBasedLerpEnumerator(float time, Func<float, float> lerpFunction,
+            CancelToken token = null)
+        {
+            float elapsed = 0f;
+            while (token && elapsed <= time)
+            {
+                // Calculate lerp from lerp function.
+                float t = lerpFunction(elapsed);
+
+                // Add delta time
+                elapsed += Time.deltaTime;
+
+                // Yield the lerp value.
+                yield return t;
+            }
+
+            // Give a lerp value of 1 so that it always finishes in a completed state.
+            yield return 1f;
         }
 
         public static void RefreshLayout(this RectTransform rt)
@@ -273,6 +354,16 @@ namespace Tactile
             float b = backgroundColor.b;
             bool useDarkColor = r * 0.299f + g * 0.587f + b * 0.114f > 186;
             return useDarkColor ? darkTextColor : lightTextColor;
+        }
+
+        public static Orientation ToLocalOrientation(this Transform transform)
+        {
+            return Orientation.LocalFromTransform(transform);
+        }
+
+        public static Orientation ToWorldOrientation(this Transform transform)
+        {
+            return Orientation.WorldFromTransform(transform);
         }
     }
 }
